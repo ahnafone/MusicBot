a9d6afa760669933b6e88b31cd34f4cbf423efa6
Ahnaf Ahmed <43047641+ahnafone@users.noreply.github.com>
2021-03-20 20:53:40 +0600

    Updates

---
+++ precache-manifest.b75a5988b19b37089aaedd7b1df6b04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c05c7bcef04a97a64d12d0472e42afc",
+    "url": "/MusicBot/index.html"
+  },
+  {
+    "revision": "f587d47aa9f2fa173eb3",
+    "url": "/MusicBot/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4c6ea01fea6a94bcf2c1",
+    "url": "/MusicBot/static/js/2.6f45710b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MusicBot/static/js/2.6f45710b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f587d47aa9f2fa173eb3",
+    "url": "/MusicBot/static/js/main.74f1af0b.chunk.js"
+  },
+  {
+    "revision": "32f1a7c41f46c0d25bf6",
+    "url": "/MusicBot/static/js/runtime-main.e306d305.js"
+  }
+]);
